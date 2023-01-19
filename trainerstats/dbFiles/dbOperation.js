f9886e2b5c4f73731b3381723b45048cc2bbc086
MichaelSault <michael_sault@hotmail.com>
2023-01-18 23:38:33 -0500

    set input values for gameplay stats

--- trainerstats/dbFiles/dbOperation.js
+++ trainerstats/dbFiles/dbOperation.js
@@ -30,6 +30,21 @@ const createTrainerProfile = async(TrainerProfile) => {
     }
 }
 
+const createGameplayEntry = async(GameplayEntry) => {
+    console.log(GameplayEntry);
+    try {
+        let pool = await sql.connect(config);
+        let trainers = await pool.request().query(`INSERT INTO TrainerProfile(TrainerName, Email, Firstname, Lastname, Password) VALUES (
+            '${GameplayEntry.trainerName}', '${GameplayEntry.email}', '${GameplayEntry.firstName}', '${GameplayEntry.lastName}', '${GameplayEntry.password}'
+        )`);
+        console.log(GameplayEntry.trainerName, GameplayEntry.email, GameplayEntry.firstName, GameplayEntry.lastName, GameplayEntry.password);
+        return trainers;
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
 
 module.exports = {
     createTrainerProfile,
