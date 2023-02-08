b07ff658f282700cb2bb76cc83f8b96e6fe7a1af
MichaelSault <michael_sault@hotmail.com>
2023-02-07 22:42:58 -0500

    added operation for creating entryID

--- trainerstats/dbFiles/dbOperation.js
+++ trainerstats/dbFiles/dbOperation.js
@@ -18,7 +18,7 @@ const getTrainerProfile = async(TrainerProfile) => {
 const getGameplayEntry = async(TrainerProfile) => {
     try {
         let pool = await sql.connect(config);
-        let trainers = await pool.request().query(`SELECT * from GameplayMedals WHERE TrainerID = '${TrainerProfile.TrainerID}'`);
+        let trainers = await pool.request().query(`SELECT * from GameplayMedals INNER JOIN Entries ON GameplayMedals.EntryID = Entries.EntryID WHERE TrainerID = '${TrainerProfile.TrainerID}'`);
         //console.log("Value returned by query:")
         //console.log(trainers);
         return trainers;
@@ -53,7 +53,7 @@ const getStatHistory = async(StatQuery) => {
 const getTrainerEntry = async(TrainerProfile) => {
     try {
         let pool = await sql.connect(config);
-        let trainers = await pool.request().query(`SELECT * from TrainerStats WHERE TrainerID = '${TrainerProfile.TrainerID}'`);
+        let trainers = await pool.request().query(`SELECT * from TrainerStats INNER JOIN Entries ON TrainerStats.EntryID = Entries.EntryID WHERE TrainerID = '${TrainerProfile.TrainerID}'`);
         console.log("Value returned by query:")
         console.log(trainers);
         return trainers;
@@ -66,10 +66,23 @@ const getTrainerEntry = async(TrainerProfile) => {
 const getTypeEntry = async(TrainerProfile) => {
     try {
         let pool = await sql.connect(config);
-        let trainers = await pool.request().query(`SELECT * from TypeMedals WHERE TrainerID = '${TrainerProfile.TrainerID}'`);
+        let trainers = await pool.request().query(`SELECT * from TypeMedals INNER JOIN Entries ON TypeMedals.EntryID = Entries.EntryID WHERE TrainerID = '${TrainerProfile.TrainerID}'`);
         console.log("Value returned by query:")
         console.log(trainers);
         return trainers;
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+const createEntry = async(TrainerProfile) => {
+    console.log(TrainerProfile);
+    try {
+        let pool = await sql.connect(config);
+        let entries = await pool.request().query(`INSERT INTO Entries(TrainerID, Date) VALUES ('${TrainerProfile.TrainerID}', '${TrainerProfile.Date}')`);
+        console.log(TrainerProfile.TrainerID, TrainerProfile.Date);
+        return entries;
     }
     catch(error) {
         console.log(error);
@@ -95,8 +108,8 @@ const createGameplayEntry = async(GameplayEntry) => {
     console.log(GameplayEntry);
     try {
         let pool = await sql.connect(config);
-        let trainers = await pool.request().query(`INSERT INTO GameplayMedals(TrainerID, Date, Kanto, Collector, Scientist, Breeder, Sightseer, PikachuFan, Johto, GymLeader, PokemonRanger, Idol, GreatLeague, Cameraman, Purifier, Triathlete, RisingStar, Picknicker, VivillonCollector, Jogger, Backpacker, Fisher, BattleGirl, Youngster, Unown, BerryMaster, Hoenn, Gentleman, Sinnoh, UltraLeagueVeteran, MasterLeagueVeteran, Unova, Hero, Kalos, Alola, Successor, RaidExpert, AceTrainer, Champion, BattleLegend, Pilot, UltraHero, RisingStarDuo, MegaEvolutionGuru, Hisui, BestBuddy, Galar, FriendFinder, TinyPokemonCollector, JumboPokemonCollector, Wayfarer) VALUES (
-            001, '2022-02-22', '${GameplayEntry.Kanto}', '${GameplayEntry.Collector}', '${GameplayEntry.Scientist}', '${GameplayEntry.Breeder}', '${GameplayEntry.Sightseer}', '${GameplayEntry.PikachuFan}', '${GameplayEntry.Johto}', '${GameplayEntry.GymLeader}', '${GameplayEntry.PokemonRanger}', '${GameplayEntry.Idol}','${GameplayEntry.GreatLeague}', '${GameplayEntry.Cameraman}', '${GameplayEntry.Purifier}', '${GameplayEntry.Triathlete}', '${GameplayEntry.RisingStar}', '${GameplayEntry.Picknicker}', '${GameplayEntry.VivillonCollector}', '${GameplayEntry.Jogger}', '${GameplayEntry.Backpacker}', '${GameplayEntry.Fisher}', '${GameplayEntry.BattleGirl}', '${GameplayEntry.Youngster}', '${GameplayEntry.Unown}', '${GameplayEntry.BerryMaster}', '${GameplayEntry.Hoenn}', '${GameplayEntry.Gentleman}', '${GameplayEntry.Sinnoh}', '${GameplayEntry.UltraLeague}', '${GameplayEntry.MasterLeague}', '${GameplayEntry.Unova}', '${GameplayEntry.Hero}', '${GameplayEntry.Kalos}', '${GameplayEntry.Alola}', '${GameplayEntry.Successor}', '${GameplayEntry.RaidExpert}', '${GameplayEntry.AceTrainer}', '${GameplayEntry.Champion}', '${GameplayEntry.BattleLegend}', '${GameplayEntry.Pilot}', '${GameplayEntry.UltraHero}','${GameplayEntry.RisingStarDuo}', '${GameplayEntry.MegaEvolutionGuru}', '${GameplayEntry.Hisui}', '${GameplayEntry.BestBuddy}', '${GameplayEntry.Galar}', '${GameplayEntry.FriendFinder}', '${GameplayEntry.TinyPokemonCollector}', '${GameplayEntry.JumboPokemonCollector}', '${GameplayEntry.Wayfarer}'
+        let trainers = await pool.request().query(`INSERT INTO GameplayMedals(Kanto, Collector, Scientist, Breeder, Sightseer, PikachuFan, Johto, GymLeader, PokemonRanger, Idol, GreatLeague, Cameraman, Purifier, Triathlete, RisingStar, Picknicker, VivillonCollector, Jogger, Backpacker, Fisher, BattleGirl, Youngster, Unown, BerryMaster, Hoenn, Gentleman, Sinnoh, UltraLeagueVeteran, MasterLeagueVeteran, Unova, Hero, Kalos, Alola, Successor, RaidExpert, AceTrainer, Champion, BattleLegend, Pilot, UltraHero, RisingStarDuo, MegaEvolutionGuru, Hisui, BestBuddy, Galar, FriendFinder, TinyPokemonCollector, JumboPokemonCollector, Wayfarer) VALUES (
+            '${GameplayEntry.Kanto}', '${GameplayEntry.Collector}', '${GameplayEntry.Scientist}', '${GameplayEntry.Breeder}', '${GameplayEntry.Sightseer}', '${GameplayEntry.PikachuFan}', '${GameplayEntry.Johto}', '${GameplayEntry.GymLeader}', '${GameplayEntry.PokemonRanger}', '${GameplayEntry.Idol}','${GameplayEntry.GreatLeague}', '${GameplayEntry.Cameraman}', '${GameplayEntry.Purifier}', '${GameplayEntry.Triathlete}', '${GameplayEntry.RisingStar}', '${GameplayEntry.Picknicker}', '${GameplayEntry.VivillonCollector}', '${GameplayEntry.Jogger}', '${GameplayEntry.Backpacker}', '${GameplayEntry.Fisher}', '${GameplayEntry.BattleGirl}', '${GameplayEntry.Youngster}', '${GameplayEntry.Unown}', '${GameplayEntry.BerryMaster}', '${GameplayEntry.Hoenn}', '${GameplayEntry.Gentleman}', '${GameplayEntry.Sinnoh}', '${GameplayEntry.UltraLeague}', '${GameplayEntry.MasterLeague}', '${GameplayEntry.Unova}', '${GameplayEntry.Hero}', '${GameplayEntry.Kalos}', '${GameplayEntry.Alola}', '${GameplayEntry.Successor}', '${GameplayEntry.RaidExpert}', '${GameplayEntry.AceTrainer}', '${GameplayEntry.Champion}', '${GameplayEntry.BattleLegend}', '${GameplayEntry.Pilot}', '${GameplayEntry.UltraHero}','${GameplayEntry.RisingStarDuo}', '${GameplayEntry.MegaEvolutionGuru}', '${GameplayEntry.Hisui}', '${GameplayEntry.BestBuddy}', '${GameplayEntry.Galar}', '${GameplayEntry.FriendFinder}', '${GameplayEntry.TinyPokemonCollector}', '${GameplayEntry.JumboPokemonCollector}', '${GameplayEntry.Wayfarer}'
         )`);
         return trainers;
     }
@@ -109,8 +122,8 @@ const createTypeEntry = async(TypeEntry) => {
     console.log(TypeEntry);
     try {
         let pool = await sql.connect(config);
-        let trainers = await pool.request().query(`INSERT INTO TypeMedals(TrainerID, Date, Schoolkid, BlackBelt, BirdKeeper, PunkGirl, RuinManiac, Hiker, BugCatcher, HexManiac, RailStaff, Kindler, Swimmer, Gardener, Rocker, Psychic, Skier, DragonTamer, Delinquent, FairyTaleGirl) VALUES (
-            001, '2022-02-22', '${TypeEntry.Schoolkid}', '${TypeEntry.BlackBelt}', '${TypeEntry.BirdKeeper}', '${TypeEntry.PunkGirl}', '${TypeEntry.RuinManiac}','${TypeEntry.Hiker}', '${TypeEntry.BugCatcher}', '${TypeEntry.HexManiac}', '${TypeEntry.RailStaff}', '${TypeEntry.Kindler}', '${TypeEntry.Swimmer}', '${TypeEntry.Gardener}', '${TypeEntry.Rocker}', '${TypeEntry.Psychic}','${TypeEntry.Skier}', '${TypeEntry.DragonTamer}', '${TypeEntry.Delinquent}', '${TypeEntry.FairyTaleGirl}'
+        let trainers = await pool.request().query(`INSERT INTO TypeMedals(Schoolkid, BlackBelt, BirdKeeper, PunkGirl, RuinManiac, Hiker, BugCatcher, HexManiac, RailStaff, Kindler, Swimmer, Gardener, Rocker, Psychic, Skier, DragonTamer, Delinquent, FairyTaleGirl) VALUES (
+            '${TypeEntry.Schoolkid}', '${TypeEntry.BlackBelt}', '${TypeEntry.BirdKeeper}', '${TypeEntry.PunkGirl}', '${TypeEntry.RuinManiac}','${TypeEntry.Hiker}', '${TypeEntry.BugCatcher}', '${TypeEntry.HexManiac}', '${TypeEntry.RailStaff}', '${TypeEntry.Kindler}', '${TypeEntry.Swimmer}', '${TypeEntry.Gardener}', '${TypeEntry.Rocker}', '${TypeEntry.Psychic}','${TypeEntry.Skier}', '${TypeEntry.DragonTamer}', '${TypeEntry.Delinquent}', '${TypeEntry.FairyTaleGirl}'
         )`);
         //console.log(TypeEntry.trainerName, TypeEntry.email, TypeEntry.firstName, TypeEntry.lastName, TypeEntry.password);
         return trainers;
@@ -124,8 +137,8 @@ const createTrainerEntry = async(TrainerStatEntry) => {
     console.log(TrainerStatEntry);
     try {
         let pool = await sql.connect(config);
-        let trainers = await pool.request().query(`INSERT INTO TrainerStats(TrainerID, Date, Caught, Seen, DistanceWalked, PokemonCaught, PokestopsVisited, TrainerLevel, TotalXP) VALUES (
-            005, '2022-02-22', '${TrainerStatEntry.Caught}', '${TrainerStatEntry.Seen}', '${TrainerStatEntry.DistanceWalked}', '${TrainerStatEntry.PokemonCaught}', '${TrainerStatEntry.PokestopsVisited}','${TrainerStatEntry.TrainerLevel}', '${TrainerStatEntry.TotalXP}'
+        let trainers = await pool.request().query(`INSERT INTO TrainerStats(Caught, Seen, DistanceWalked, PokemonCaught, PokestopsVisited, TrainerLevel, TotalXP) VALUES (
+            '${TrainerStatEntry.Caught}', '${TrainerStatEntry.Seen}', '${TrainerStatEntry.DistanceWalked}', '${TrainerStatEntry.PokemonCaught}', '${TrainerStatEntry.PokestopsVisited}','${TrainerStatEntry.TrainerLevel}', '${TrainerStatEntry.TotalXP}'
         )`);
         //console.log(TypeEntry.trainerName, TypeEntry.email, TypeEntry.firstName, TypeEntry.lastName, TypeEntry.password);
         return trainers;
@@ -137,6 +150,7 @@ const createTrainerEntry = async(TrainerStatEntry) => {
 
 
 module.exports = {
+    createEntry,
     createTrainerProfile,
     getGameplayEntry,
     getStatHistory,
